b27cd75f59ef25b0d4760c0f152f2c0c4a5a7c84
Egor <Egor@DESKTOP-GC3R1TS>
2023-10-16 18:33:44 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string card = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            card = "Quin of diamonds";
+            textBox1.Text = card;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            card = "7 of clubs";
+            textBox1.Text = card;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            card = "Ace of diamonds";
+            textBox1.Text = card;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            card = "King of hearts";
+            textBox1.Text = card;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
